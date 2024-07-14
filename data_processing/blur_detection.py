c549eaef31be327ac88ec1289e25df74a719d30e
mga113 <mga113@cs-3dlg-01.cmpt.sfu.ca>
2024-07-14 13:45:17 -0700

    autoformatted code

--- data_processing/blur_detection.py
+++ data_processing/blur_detection.py
@@ -4,47 +4,45 @@ import shutil
 import argparse
 import cv2
 
-""" 
-NOTE:
-The cropped images are evaluated for blurriness because we are interested in the clarity of the insects within the bounding boxes, 
-not the entire image.This approach ensures that the model trains on and identifies whole, original images as clear or 
-blurry based on the clarity of the insects in the bounding boxes.
 """
-# Computes the variance of the Laplacian of the image to determine its blurriness.
+NOTE:
+The cropped images are evaluated for blurriness because
+we are interested in the clarity of the insects within the bounding boxes,
+not the entire image.This approach ensures that the model trains on and
+identifies whole, original images as clear or blurry based on the clarity
+of the insects in the bounding boxes.
+"""
+
+
+# Computes Laplacian variance to measure image blurriness
 def variance_of_laplacian(image):
     return cv2.Laplacian(image, cv2.CV_64F).var()
+
 
 # Loads the JSON file that maps cropped images to their original images.
 def load_image_mapping(mapping_file):
     with open(mapping_file) as f:
         return json.load(f)
 
+
 # Creates directories for storing processed original images.
 def create_output_directories(output_dir):
 
-    # create good and bad image directories 
+    # create good and bad image directories
     bad_images_dir = os.path.join(output_dir, "bad_imgs")
     good_images_dir = os.path.join(output_dir, "good_imgs")
 
-    # Places the blurry images in the bad and clear in good directories respectively
-    #original_blurry_dir = os.path.join(bad_images_dir, "original_blurry_images")
-    #original_clear_dir = os.path.join(good_images_dir, "original_clear_images")
-
-    #os.makedirs(original_blurry_dir, exist_ok=True)
-    #os.makedirs(original_clear_dir, exist_ok=True)
     os.makedirs(good_images_dir, exist_ok=True)
     return bad_images_dir, good_images_dir
 
-def count_images(directory):
-    return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
 
-# Processes images to classify them as blurry or clear based on Laplacian variance.
+# Processes and classifies images as blurry or clear based on variance.
 def process_images(output_dir, image_mapping, threshold, annotations):
     original_blurry_dir, original_clear_dir = create_output_directories(output_dir)
     original_clear_annotations = {"images": [], "annotations": []}
 
     num_original_bad = len(os.listdir(original_blurry_dir))
-    #print(num_original_bad)
+    # print(num_original_bad)
 
     for cropped_path, original_path in image_mapping.items():
         try:
@@ -59,12 +57,23 @@ def process_images(output_dir, image_mapping, threshold, annotations):
                 destination_dir = original_clear_dir
 
                 # Find the corresponding image data in annotations
-                image_data = next((img for img in annotations["images"] if img["file_name"] == original_filename), None)
-                
-                # Add image and annotations to original_clear_annotations if the image is clear
+                image_data = next(
+                    (
+                        img
+                        for img in annotations["images"]
+                        if img["file_name"] == original_filename
+                    ),
+                    None,
+                )
+
+                # If clear, add image and annotation to clear set
                 if image_data:
                     original_clear_annotations["images"].append(image_data)
-                    img_annotations = [ann for ann in annotations["annotations"] if ann["image_id"] == image_data["id"]]
+                    img_annotations = [
+                        ann
+                        for ann in annotations["annotations"]
+                        if ann["image_id"] == image_data["id"]
+                    ]
                     original_clear_annotations["annotations"].extend(img_annotations)
 
             shutil.copy(original_path, os.path.join(destination_dir, original_filename))
@@ -76,33 +85,59 @@ def process_images(output_dir, image_mapping, threshold, annotations):
     num_original_clear = len(os.listdir(original_clear_dir))
 
     # Save the annotations for clear images to a JSON file
-    original_clear_annotations_file = os.path.join(original_clear_dir, "original_clear_annotations.json")
+    original_clear_annotations_file = os.path.join(
+        original_clear_dir, "original_clear_annotations.json"
+    )
     with open(original_clear_annotations_file, "w") as f:
         json.dump(original_clear_annotations, f, indent=4)
 
     return num_original_blurry, num_original_clear
 
-# load the image mapping file, process images as clear or blurry 
+
+# load the image mapping file, process images as clear or blurry
 def main(args):
     image_mapping = load_image_mapping(args.mapping_file)
-    
+
     # Load the annotations
     with open(os.path.join(args.input_dir, "coco_annotations_processed.json")) as f:
         annotations = json.load(f)
-        
-    num_blurry, num_clear = process_images(args.output_dir, image_mapping, args.threshold, annotations)
+
+    num_blurry, num_clear = process_images(
+        args.output_dir, image_mapping, args.threshold, annotations
+    )
     print(f"Number of original blurry images: {num_blurry}")
     print(f"Number of original clear images: {num_clear}")
 
 
 if __name__ == "__main__":
 
-    # Define file locations 
+    # Define file locations
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_dir", required=True, help="Path to input directory of cropped images")
-    parser.add_argument("-m", "--mapping_file", required=True, help="Path to JSON mapping file of cropped to original images")
-    parser.add_argument("-o", "--output_dir", required=True, help="Path to output directory of clear and blurry images")
-    parser.add_argument("-t", "--threshold", type=float, default=130.0, help="Focus measures that fall below this value will be considered 'blurry'")
+    parser.add_argument(
+        "-i",
+        "--input_dir",
+        required=True,
+        help="Path to input directory of cropped images",
+    )
+    parser.add_argument(
+        "-m",
+        "--mapping_file",
+        required=True,
+        help="Path to JSON mapping file of cropped to original images",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        required=True,
+        help="Path to output directory of clear and blurry images",
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        type=float,
+        default=130.0,
+        help="Focus measures falling below this are considered 'blurry'",
+    )
     args = parser.parse_args()
 
     # Implement blur detection
--- data_processing/crop_images.py
+++ data_processing/crop_images.py
@@ -4,96 +4,129 @@ import shutil
 from PIL import Image
 import argparse
 
-# Load all image annotations 
+
+# Load all image annotations
 def load_annotations(input_dir):
     with open(os.path.join(input_dir, "coco_annotations_processed.json")) as f:
         return json.load(f)
 
-# Create directories to store cropped and unbounded images 
+
+# Create directories to store cropped and unbounded images
 def create_directories(cropped_output_dir, unbounded_output_dir):
     os.makedirs(cropped_output_dir, exist_ok=True)
     os.makedirs(unbounded_output_dir, exist_ok=True)
 
-# Crop the image, store in respective directories, map cropped image path to original 
-def crop_image(image_path, img_annotations, cropped_output_dir, unbounded_output_dir, image_mapping):
+
+# Crop image, store in respective directories, map cropped image to original
+def crop_image(
+    image_path, img_annotations, cropped_output_dir, unbounded_output_dir, image_mapping
+):
 
     # Check if the image path is valid
     if not os.path.exists(image_path):
-        #print(f"Image {os.path.basename(image_path)} not found.")
-        print(image_path)
+        print(f"Image {os.path.basename(image_path)} not found.")
         return
 
-    # Open the image 
+    # Open the image
     image = Image.open(image_path)
 
     # Flag to check if the image is unbounded
     is_unbounded = True
 
-    # Loop through each image's annotations 
+    # Loop through each image's annotations
     for ann in img_annotations:
         bbox = ann["bbox"]
         left, top, width, height = bbox
 
-        # Move images to the unbounded directory if it does not have a valid bounding box 
+        # Move images to the unbounded directory if it does not have a valid bounding box
         if width > 0 and height > 0:
             is_unbounded = False
             right, bottom = left + width, top + height
             cropped_image = image.crop((left, top, right, bottom))
 
-            # Create the cropped image filename, move to the cropped directory, and save its path 
+            # Create the cropped image filename, move to the cropped directory, and save its path
             cropped_image_filename = f"{ann['image_id']}_{ann['id']}_cropped.jpg"
-            cropped_image_path = os.path.join(cropped_output_dir, cropped_image_filename)
+            cropped_image_path = os.path.join(
+                cropped_output_dir, cropped_image_filename
+            )
             cropped_image.save(cropped_image_path)
 
-            # Save the cropped image path to the original 
+            # Save the cropped image path to the original
             image_mapping[cropped_image_path] = image_path
 
     # If the image is unbounded, move it to the unbounded directory
     if is_unbounded:
-        shutil.copy(image_path, os.path.join(unbounded_output_dir, os.path.basename(image_path)))
+        shutil.copy(
+            image_path, os.path.join(unbounded_output_dir, os.path.basename(image_path))
+        )
 
-# Save the cropped to original path mappings in a json file 
+
+# Save the cropped to original path mappings in a json file
 def save_image_mapping(cropped_output_dir, image_mapping):
     with open(os.path.join(cropped_output_dir, "image_mapping.json"), "w") as f:
         json.dump(image_mapping, f, indent=4)
 
+
 # Crop the image, store image in respective directories, create image mapping json file
 def process_image(args):
 
-    # Load the image annotations 
+    # Load the image annotations
     coco_annotation_dict = load_annotations(args.input_dir)
-    images, annotations = coco_annotation_dict["images"], coco_annotation_dict["annotations"]
+    images, annotations = (
+        coco_annotation_dict["images"],
+        coco_annotation_dict["annotations"],
+    )
 
-    # Create cropped and unbounded directories for image storage 
+    # Create cropped and unbounded directories for image storage
     create_directories(args.cropped_output_dir, args.unbounded_output_dir)
 
-    # Initialize image path mapping dictionary 
+    # Initialize image path mapping dictionary
     image_mapping = {}
     for img in images:
         image_id = img["id"]
         image_path = os.path.join(args.input_dir, img["file_name"])
-        # Ensure that each image has a corresponding annotation 
+        # Ensure that each image has a corresponding annotation
         img_annotations = [ann for ann in annotations if ann["image_id"] == image_id]
-        
-        # Crop the image and map the cropped path to the original 
-        crop_image(image_path, img_annotations, args.cropped_output_dir, args.unbounded_output_dir, image_mapping)
-    
-    # Save the image mappings into a json file 
+
+        # Crop the image and map the cropped path to the original
+        crop_image(
+            image_path,
+            img_annotations,
+            args.cropped_output_dir,
+            args.unbounded_output_dir,
+            image_mapping,
+        )
+
+    # Save the image mappings into a json file
     save_image_mapping(args.cropped_output_dir, image_mapping)
 
     num_unbounded = len(os.listdir(args.unbounded_output_dir))
     num_cropped = len(os.listdir(args.cropped_output_dir))
 
-    
     print(num_cropped, f"Cropped images saved in {args.cropped_output_dir}")
     print(num_unbounded, f"Unbounded images saved in {args.unbounded_output_dir}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_dir", type=str, required=True, help="Path to the folder that contains the images and COCO file.")
-    parser.add_argument("--cropped_output_dir", type=str, required=True, help="Path to save cropped images.")
-    parser.add_argument("--unbounded_output_dir", type=str, required=True, help="Path to save unbounded images.")
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        required=True,
+        help="Path to the folder that contains the images and COCO file.",
+    )
+    parser.add_argument(
+        "--cropped_output_dir",
+        type=str,
+        required=True,
+        help="Path to save cropped images.",
+    )
+    parser.add_argument(
+        "--unbounded_output_dir",
+        type=str,
+        required=True,
+        help="Path to save unbounded images.",
+    )
     args = parser.parse_args()
 
     process_image(args)
-
